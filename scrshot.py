7b8b75c5094cd6d5eb01341a6635e64393263037
Mr. Robot <96381965+Casanova5065@users.noreply.github.com>
2023-02-03 17:39:58 +0530

    Update scrshot.py

--- scrshot.py
+++ scrshot.py
@@ -4,19 +4,22 @@ import datetime
 import tempfile
 import os
 from pyautogui import screenshot 
+from time import sleep
 
 def takeScreenshot():
     try:
-        myScreenshot = screenshot()
-        path_to_dir = tempfile.gettempdir() + "/screenshots"
-        path_to_logfile = path_to_dir + "/screenshots.log"
+       
+        path_to_dir = tempfile.gettempdir() + "\\screenshots"
+        path_to_logfile = path_to_dir + "\\screenshots.log"
         letters = string.ascii_lowercase
         name = ''.join(random.choice(letters) for i in range(10))
         
         if os.path.exists(path_to_dir) == False:
             os.mkdir(path_to_dir)
+            sleep(2)
             
-        myScreenshot.save(path_to_dir + "/" + name + '.png')
+        myScreenshot = screenshot()
+        myScreenshot.save(path_to_dir + "\\" + name + '.png')
         # should be saved to temp dir
         with open(path_to_logfile, "a") as file:
             file.write(str(datetime.datetime.now()) + "\n")
